beab581aab37ec8b300e22a5c769d9ae08c8458d
Tuhin Sengupta <tuhin.sengupta@tallysolutions.com>
2021-08-09 14:31:59 +0530

    ACript updated

--- Install-Qt.sh
+++ Install-Qt.sh
@@ -2,50 +2,79 @@
 
 echo "INFO:Installing Qt ....  "
 
-OSName=`uname -r`
+OSName=`uname`
+
+echo "INFO: OSName = $OSName  "
+
 
 #Set QTHome based on OS
-$QtHOME=$env:QT_HOME
+if [ "$OSName" == "Darwin" ]
+then
+  OS=MacOS
+  QT_HOME=/Users/Shared/Qt
+  echo "INFO: Installing Qt for MacOS Location : $QT_HOME ..."
+fi
+if [ "$OSName" == "Linux" ]
+then
+  OS=Linux
+  QT_HOME=/usr/share/Qt
+  echo "INFO: Installing Qt for Linux Location : $QT_HOME ..."
+fi
 
-#Set OS Version
-$QtVersion="6.1.0"
+#Set QT Version
+QT_VERSION=6.1.0
 
 
 #Set Host Arch - x64 or ARM 64 
+ARCH=`arch`
+if [ "arm64"  == `arch` ]
+then
+  HOST_ARCH=ARM64
+fi
 
-if ( $env:PROCESSOR_ARCHITECTURE -eq "AMD64" )
-{
-    $host_arch="x64"
-}else{
-    $host_arch="ARM64"
+if [ "x86_64"  == `arch` ]
+then
+  HOST_ARCH=x64
+fi
+
+DownloadQtFile() {
+  OS=$1
+  COMP=$2
+  ARCH=$3
+
+  echo "INFO: Downloading Qt component $COMP for $OS/$ARCH ..."
 }
 
-
 #Download Base zip file
-DownloadQtFile $OS "Base" $host_arch
+DownloadQtFile $OS "Base" $HOST_ARCH
 
-#Download Qt libs for  both architecture
+#Download Qt libs/Includes for  both architecture
 
-#IF MacOS, download MacOS and iOS
-DownloadQtFile MacOS "Libs" "x64"
-DownloadQtFile MacOS "Libs" "ARM64"
-DownloadQtFile iOS "Libs" "x64"
-DownloadQtFile iOS "Libs" "ARM64"
+if [ "$OS" == "MacOS" ]
+then
+    #IF MacOS, download MacOS and iOS
 
-#IF Linux, download Linux
-DownloadQtFile "Linux" "Libs" "x64"
-DownloadQtFile "Linux" "Libs" "ARM64"
+    DownloadQtFile MacOS "Libs" "x64"
+    DownloadQtFile MacOS "Libs" "ARM64"
+    DownloadQtFile iOS "Libs" "x64"
+    DownloadQtFile iOS "Libs" "ARM64"
 
-#Download Qt uncludes for  both architecture
+    DownloadQtFile MacOS "Includes" "x64"
+    DownloadQtFile MacOS "Includes" "ARM64"
+    DownloadQtFile iOS "Includes" "x64"
+    DownloadQtFile iOS "Includes" "ARM64"
+fi
 
-#IF MacOS, download MacOS and iOS
-DownloadQtFile MacOS "Includes" "x64"
-DownloadQtFile MacOS "Includes" "ARM64"
-DownloadQtFile iOS "Includes" "x64"
-DownloadQtFile iOS "Includes" "ARM64"
+if [ "$OS" == "Linux" ]
+then
+    #IF Linux, download Linux
 
-#IF Linux, download Linux
-DownloadQtFile "Linux" "Includes" "x64"
-DownloadQtFile "Linux" "Includes" "ARM64"
+    DownloadQtFile "Linux" "Libs" "x64"
+    DownloadQtFile "Linux" "Libs" "ARM64"
+
+    DownloadQtFile "Linux" "Includes" "x64"
+    DownloadQtFile "Linux" "Includes" "ARM64"
+fi
+
 
 echo "INFO:Installing of Qt Completed Successfully.. "
