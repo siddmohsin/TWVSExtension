c0090874121359e4ab1883c63db490d9685baf20
Tuhin Sengupta <tuhin.sengupta@tallysolutions.com>
2021-11-08 17:49:06 +0530

    new bootstrap json

--- wsl-packages-ubuntu20.04.sh
+++ wsl-packages-ubuntu20.04.sh
@@ -272,11 +272,11 @@ fi
 #install CMake 3.19.0
 echo "INFO:Installing Latest CMake  ...  "
 
-sudo apt remove --purge --auto-remove cmake
+sudo apt remove --purge --auto-remove cmake -y
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
-sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
-sudo apt update
-sudo apt install cmake
+sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main' -y
+sudo apt update -y
+sudo apt install cmake -y
 
 if [ $? != 0 ]
 then
